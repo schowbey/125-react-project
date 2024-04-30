6879f32641d4dbcffdd3b85ba61f3021c1549a0f
schowbey <103155665+schowbey@users.noreply.github.com>
2024-04-30 11:40:03 -0700

    testing it out

---
+++ home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page of my website.</p>
+      <p>Feel free to explore and learn more about what we have to offer.</p>
+      <a href="/about">Learn More</a>
+    </div>
+  );
+};
+
+export default HomePage;
