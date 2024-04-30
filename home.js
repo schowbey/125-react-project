def012b3032d7c7300d407503a5560a9a1f03e40
schowbey <103155665+schowbey@users.noreply.github.com>
2024-04-30 11:44:34 -0700

    practice change

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div>
       <h1>Welcome to My Website</h1>
       <p>This is the home page of my website.</p>
-      <p>Feel free to explore and learn more about what we have to offer.</p>
+      <p>I am a 3rd-year cognitive and computer science student.</p>
       <a href="/about">Learn More</a>
     </div>
   );
